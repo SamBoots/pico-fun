d09aa2febdbb85b2ca9f49e0248447a50e25f46c
Sam <samboots123@gmail.com>
2026-05-18 22:39:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 
 #include "app/app.h"
 #include "app/ammo_counter.h"
+#include "app/snake.h"
 
 static render_context_t g_render_ctx;
 static memory_arena_t g_memory_arena;
@@ -18,7 +19,7 @@ int main(void)
     stdio_init_all();
     render_init_context(&g_render_ctx, TEST_W, TEST_H, 125);
     app_context_t app;
-    ammo_counter_init_app(&app, &g_memory_arena, &g_render_ctx);
+    snake_init_app(&app, &g_memory_arena, &g_render_ctx);
     while (true) 
     {
         uint32_t now_ms = to_ms_since_boot(get_absolute_time());
