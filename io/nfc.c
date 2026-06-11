ac98ced4b5207759428131dd7b8b41f142e6d6ba
Sam <samboots123@gmail.com>
2026-06-11 23:59:41 +0200

    NFC boilerplate

---
+++ io/nfc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "drivers/pn532.h"
+#include "nfc.h"
+
+bool nfc_init(nfc_context_t* a_ctx, nfc_connect_type_t* a_connect_type, nfc_driver_t a_driver)
+{
+    switch (a_driver)
+    {
+    case DRIVER_PN532:
+    {
+        return pn532_init(a_ctx, a_connect_type);
+        break;
+    }
+}
+
+bool nfc_read(const nfc_context_t* a_ctx, uint8_t a_offset, void* a_data, uint32_t a_len)
+{
+    return a_ctx->read(a_ctx, a_offset, a_data, a_len);
+}
+
+bool nfc_write(nfc_context_t* a_ctx, uint8_t a_offset, const void* a_data, uint32_t a_len)
+{
+    return a_ctx->write(a_ctx, a_offset, a_data, a_len);
+}
+
+bool nfc_detect_tag(const nfc_context_t* a_ctx)
+{
+    return a_ctx->detect_tag(a_ctx);
+}
---
+++ io/nfc.h
@@ -0,0 +1,34 @@
+#ifndef NFC_H
+#define NFC_H
+
+typedef enum nfc_driver_t
+{
+    DRIVER_PN532
+} nfc_driver_t;
+
+typedef struct nfc_init_info_t
+{
+    uint pin_sda;
+    uint pin_scl;
+    i2c_inst_t* i2c;
+} nfc_init_info_t;
+
+typedef struct nfc_context_t
+{
+    uint8_t page_size;  // 4 bytes NTAG or 16 for MIFARE
+    uint8_t address;
+    uint pin_sda;
+    uint pin_scl;
+    i2c_inst_t* i2c;
+
+    bool (*read)(const nfc_context_t* a_ctx, uint8_t a_offset, void* a_data, uint32_t a_len);
+    bool (*write)(const nfc_context_t* a_ctx, uint8_t a_offset, const void* a_data, uint32_t a_len);
+    bool (*detect_tag)(const nfc_context_t* a_ctx);
+} nfc_context_t;
+
+bool nfc_init(nfc_context_t* a_ctx, nfc_connect_type_t* a_connect_type, nfc_driver_t a_driver);
+bool nfc_read(const nfc_context_t* a_ctx, uint8_t a_offset, void* a_data, uint32_t a_len);
+bool nfc_write(nfc_context_t* a_ctx, uint8_t a_offset, const void* a_data, uint32_t a_len);
+bool nfc_detect_tag(const nfc_context_t* a_ctx);
+
+#endif // NFC_H
