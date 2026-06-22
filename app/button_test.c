a0675e2df457e1317d662c187d3fbf9162247469
Sam Boots <samboots123@gmail.com>
2026-06-22 17:51:30 +0200

    button_test app prototype

---
+++ app/button_test.c
@@ -0,0 +1,50 @@
+#include "pico/stdlib.h"
+#include "app.h"
+#include "button_test.h"
+#include "../io/io_types.h"
+#include "../io/button.h"
+#include "../memory/memory_arena.h"
+#include "math.h"
+
+typedef struct button_test_context_t
+{
+    button_context_t button_0;
+    button_context_t button_1;
+    button_context_t button_2;
+    button_context_t button_3;
+} button_test_context_t;
+
+// simple sine wave tone, 4000 samples at 8000Hz = 0.5 seconds
+static uint8_t s_tone[4000];
+
+void button_test_init_app(app_context_t* a_app, memory_arena_t* a_arena, render_context_t* a_ctx, void* a_app_params)
+{
+    a_app->memory_arena_marker = memory_arena_get_marker(a_arena);
+    a_app->user_data = memory_arena_allocate(a_arena, sizeof(pwm_test_context_t));
+    a_app->update = button_test_update;
+    a_app->render = button_test_render;
+    a_app->close = button_test_close;
+    
+
+    button_test_context_t* app_ctx = (button_test_context_t*)a_app->user_data;
+    button_init_context(&app_ctx->pwm, 13);
+}
+
+app_update_status_t button_test_update(app_context_t* a_app, memory_arena_t* a_arena, render_context_t* a_ctx, uint32_t a_now_ms)
+{
+    button_test_context_t* app_ctx = (button_test_context_t*)a_app->user_data;
+    
+
+    // never render
+    return APP_OK;
+}
+
+void button_test_render(app_context_t* a_app, render_context_t* a_ctx)
+{
+    button_test_context_t* app_ctx = (button_test_context_t*)a_app->user_data;
+}
+
+void button_test_close(app_context_t* a_app)
+{
+    button_test_context_t* app_ctx = (button_test_context_t*)a_app->user_data;
+}
---
+++ app/button_test.h
@@ -0,0 +1,18 @@
+#ifndef BUTTON_TEST_H
+#define BUTTON_TEST_H
+
+typedef struct memory_arena_t memory_arena_t;
+typedef struct render_context_t render_context_t;
+typedef struct app_context_t app_context_t;
+
+typedef struct button_test_params_t
+{
+    uint16_t pad;
+} button_test_params_t;
+
+void button_test_init_app(app_context_t* a_app, memory_arena_t* a_arena, render_context_t* a_ctx, void* a_app_params);
+app_update_status_t button_test_update(app_context_t* a_app, memory_arena_t* a_arena, render_context_t* a_ctx, uint32_t a_now_ms);
+void button_test_render(app_context_t* a_app, render_context_t* a_ctx);
+void button_test_close(app_context_t* a_app);
+
+#endif // BUTTON_TEST_H
